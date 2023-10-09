4cf38553f7d168830308c629ae4836e0cc3d4b76
Majordomi07 <majordomi07@gmail.com>
2023-10-09 19:05:35 +0200

    minden is

---
+++ frontend/typeWriter.js
@@ -0,0 +1,12 @@
+var typing = new Typed(".typeWriter", {
+  strings: [
+    "Építsd a jövődet!",
+    "Fejlődj a munka útján!",
+    "Építsd a karrieredet!",
+    "Dolgozz a céljaidért!",
+  ],
+  typeSpeed: 100,
+  backSpeed: 40,
+  backDelay: 800,
+  loop: true,
+});
