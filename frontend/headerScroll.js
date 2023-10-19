b69ae90602adaae4520765381fd8241d4073e8f4
Majordomi07 <majordomi07@gmail.com>
2023-10-19 12:29:49 +0200

    nem tudom

--- frontend/headerScroll.js
+++ frontend/headerScroll.js
@@ -6,14 +6,13 @@ function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.querySelector(".desktop").style.position = "sticky";
     document.querySelector(".desktop").style.top = "20px";
-    document.querySelector(".desktop").style.backgroundColor = "#ffffff40";
+    document.querySelector(".desktop").style.backgroundColor = "#0000005d";
     document.querySelector(".desktop").style.backdropFilter = "blur(20px)";
     document.querySelector(".desktop").style.paddingTop = "0";
     document.querySelector(".desktop").style.padding = "15px";
     document.querySelector(".desktop").style.borderRadius = "10px";
     document.querySelector(".desktop .brand img").style.height = "50px";
-    document.querySelector(".desktop").style.boxShadow =
-      "0px 4px 4px 0px rgba(0, 0, 0, 0.1)";
+    document.querySelector(".desktop").style.boxShadow = "0px 4px 4px 0px rgba(0, 0, 0, 0.1)";
   } else {
     document.querySelector(".desktop").style.position = "static";
     document.querySelector(".desktop").style.top = "0px";
