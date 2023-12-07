372e1cccb590e3984102ed424ffa827b0a0595fe
Dominik Ábel Major <major.dominik@bolyai.local>
2023-12-07 09:27:20 +0100

    minden

--- ujmelo.sql
+++ ujmelo.sql
@@ -1,10 +1,27 @@
 CREATE DATABASE ujmelo;
 
+USE ujmelo;
+
+CREATE TABLE users (
+  userID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255),
+  password VARCHAR(128),
+  firstName VARCHAR(50),
+  lastName VARCHAR(50)
+);
+
 CREATE TABLE companies (
   companiesID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   name VARCHAR(40),
   logo VARCHAR(255),
-  description TEXT(100)
+  description TEXT(100),
+  userID INT,
+  FOREIGN KEY (userID) REFERENCES users(userID)
+);
+
+CREATE TABLE category (
+  categoryID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  category VARCHAR(20)
 );
 
 CREATE TABLE advertisement (
@@ -22,23 +39,11 @@ CREATE TABLE advertisement (
   FOREIGN KEY (categoryID) REFERENCES category(categoryID)
 );
 
-CREATE TABLE category (
-  categoryID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  category VARCHAR(20)
-);
-
-CREATE TABLE users (
-  userID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255),
-  password VARCHAR(128),
-  firstName VARCHAR(50),
-  lastName VARCHAR(50),
-  admin TINYINT
-);
-
 CREATE TABLE applications (
   userID INT,
   advertisementID INT,
+  cv VARCHAR(255),
+  motivation_letter VARCHAR(255),
   FOREIGN KEY (userID) REFERENCES users(userID),
   FOREIGN KEY (advertisementID) REFERENCES advertisement(advertisementID)
 );
