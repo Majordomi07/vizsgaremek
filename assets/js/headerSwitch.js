55b2b89f758cbf163047a878e0268ba4c090f2e2
Dominik Ábel Major <major.dominik@BOLYAI.LOCAL>
2023-12-07 13:56:38 +0100

    minden

--- assets/js/headerSwitch.js
+++ assets/js/headerSwitch.js
@@ -9,6 +9,7 @@ function toggle() {
     nav.style.right = "0px";
     document.querySelector("body").style.overflow = "hidden";
     document.querySelector("html").style.overflow = "hidden";
+    bars.classList.add("active");
     open = true;
   } else {
     open = false;
@@ -16,5 +17,6 @@ function toggle() {
     nav.style.right = "-100px";
     document.querySelector("body").style.overflow = "visible";
     document.querySelector("html").style.overflow = "visible";
+    bars.classList.remove("active");
   }
 }
